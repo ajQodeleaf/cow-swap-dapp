be6a8bc1235593553e5f606558b125f52e37dc2c
Aradhya  Jain <jaradhya.in@gmail.com>
2023-12-19 12:21:41 +0530

    Refactored layout1

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,15 +7,13 @@ export default function RootLayout({ children }) {
   return (
     <WalletProvider>
       <DAppProvider config={config}>
-        <html lang="en" style={{ zoom: "100%" }}>
+        <html lang="en" style={{ zoom: "67%" }}>
         <head>
             <meta name="viewport" content="width=device-width, initial-scale=0.67" />
           </head>
           <body
             style={{
               overflow: "hidden",
-              transform: "scale(1.0)",
-              transformOrigin: "top left",
               marginLeft: "40px",
               marginRight: "40px",
               backgroundImage:
